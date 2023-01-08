9811b32e17089918b6d24017a37951e17a0f209d
Inject developer <inject.dev@gmail.com>
2023-01-08 13:29:47 +0500

    Init commit branch main

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './qiwi-p2p.module';
 export * from './qiwi-p2p.constants';
-export * from './qiwi-p2p.decorator';
 export * from './qiwi-p2p.interfaces';
 export * from './qiwi-p2p.module';
 export * from './qiwi-p2p.service';
